7a9ddc790a6b9562bb8d826677cedab609b7ae50
Jorge <info@jorgebenitezlopez.com>
2024-06-13 21:53:33 +0200

    Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import Character from "./Character";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Link, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   const vitevar = import.meta.env.VITE_VAR;
 
   return (
@@ -18,6 +20,10 @@ function App() {
         </a>
       </div>
       <h1>Vite + React + {vitevar}</h1>
+      <Routes>
+        <Route path="/character" element={<Character />} />
+      </Routes>
+      <Link to="character">Personaje</Link>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -30,7 +36,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Character.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Character: React.FC = () => {
+  return (
+    <h1>Character</h1>
+  );
+}
+
+export default Character;
