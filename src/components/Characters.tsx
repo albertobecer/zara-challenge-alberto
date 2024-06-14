9aae13d1aa83ea6b1a9590ae2654f11f53946a0e
Jorge <info@jorgebenitezlopez.com>
2024-06-14 13:19:37 +0200

    Load Characters

--- src/components/Characters.tsx
+++ src/components/Characters.tsx
@@ -1,11 +1,31 @@
 import React from "react";
+import useFetch from "../hooks/useFetch";
+
+interface Character {
+  id: number;
+  name: string;
+}
+interface ApiResponse {
+  loading: boolean;
+  error: { message: string } | null;
+  data: { results: Character[] } | null;
+}
 
 const Characters: React.FC = () => {
+  const VITE_ENDPOINT_CHARACTERS = import.meta.env.VITE_ENDPOINT_CHARACTERS;
+  const characters = useFetch<ApiResponse>(VITE_ENDPOINT_CHARACTERS);
 
   return (
     <>
-      <h1>Characters</h1>
+      {characters.loading && <p>Loading...</p>}
+      {characters.error && <p>Error: {characters.error.message}</p>}
       <br />
+
+      <ul>
+        {characters.data?.results.map((character: Character) => (
+          <li key={character.id}>{character.name}</li>
+        ))}
+      </ul>
     </>
   );
 };
