d55ba00600a848244653b6104e15b27abe17ed28
Jorge <info@jorgebenitezlopez.com>
2024-06-14 22:47:09 +0200

    Link in logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,54 +6,56 @@ import darkIcon from "/dark.svg";
 import marvelLogo from "/marvel.svg";
 
 const Navbar: React.FC = () => {
-  const location = useLocation();
-  const { theme, setTheme } = useTheme();
-  const isCharacterPage = /^\/character\/\d+$/.test(location.pathname);
+          const location = useLocation();
+          const { theme, setTheme } = useTheme();
+          const isCharacterPage = /^\/character\/\d+$/.test(location.pathname);
 
-  return (
-    <nav className={theme}>
-      <img
-        src={marvelLogo}
-        className={theme === "dark" ? "logo aux-dark-mode" : "logo"}
-        alt="Marvel logo"
-      />
+          return (
+                    <nav className={theme}>
+                              <Link to="/">
+                                        <img
+                                                  src={marvelLogo}
+                                                  className={theme === "dark" ? "logo aux-dark-mode" : "logo"}
+                                                  alt="Marvel logo"
+                                        />
+                              </Link>
 
-      <ul>
-        {location.pathname !== "/" && (
-          <li>
-            <Link to="/">Characters</Link>
-          </li>
-        )}
-        {!isCharacterPage && (
-          <li>
-            <Link to="/character/1">Character</Link>
-          </li>
-        )}
-      </ul>
-      <div className="theme-toggle">
-        <button
-          type="button"
-          className={theme === "light" ? "show" : "hidden"}
-          aria-label="Use Dark Mode"
-          onClick={() => setTheme("dark")}
-        >
-          <img
-            src={darkIcon}
-            className={theme === "dark" ? "logo aux-dark-mode" : "logo"}
-            alt="Dark icon"
-          />
-        </button>
-        <button
-          type="button"
-          className={theme === "dark" ? "show" : "hidden"}
-          aria-label="Use Light Mode"
-          onClick={() => setTheme("light")}
-        >
-          <img src={lightIcon} className={"logo"} alt="Light icon" />
-        </button>
-      </div>
-    </nav>
-  );
+                              <ul>
+                                        {location.pathname !== "/" && (
+                                                  <li>
+                                                            <Link to="/">Characters</Link>
+                                                  </li>
+                                        )}
+                                        {!isCharacterPage && (
+                                                  <li>
+                                                            <Link to="/character/1">Character</Link>
+                                                  </li>
+                                        )}
+                              </ul>
+                              <div className="theme-toggle">
+                                        <button
+                                                  type="button"
+                                                  className={theme === "light" ? "show" : "hidden"}
+                                                  aria-label="Use Dark Mode"
+                                                  onClick={() => setTheme("dark")}
+                                        >
+                                                  <img
+                                                            src={darkIcon}
+                                                            className={theme === "dark" ? "logo aux-dark-mode" : "logo"}
+                                                            alt="Dark icon"
+                                                  />
+                                        </button>
+                                        <button
+                                                  type="button"
+                                                  className={theme === "dark" ? "show" : "hidden"}
+                                                  aria-label="Use Light Mode"
+                                                  onClick={() => setTheme("light")}
+                                        >
+                                                  <img src={lightIcon} className={"logo"} alt="Light icon" />
+                                        </button>
+                              </div>
+                    </nav>
+          );
 };
 
 export default Navbar;
