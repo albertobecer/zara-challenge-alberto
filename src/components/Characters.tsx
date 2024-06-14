20d1fde7579471eb6d8a94dec23e1542e847862e
Jorge <info@jorgebenitezlopez.com>
2024-06-14 14:01:19 +0200

    Searcher

--- src/components/Characters.tsx
+++ src/components/Characters.tsx
@@ -1,33 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import useFetch from "../hooks/useFetch";
 
 interface Character {
-  id: number;
-  name: string;
+          id: number;
+          name: string;
 }
+
 interface ApiResponse {
-  loading: boolean;
-  error: { message: string } | null;
-  data: { results: Character[] } | null;
+          loading: boolean;
+          error: { message: string } | null;
+          data: { results: Character[] } | null;
 }
 
 const Characters: React.FC = () => {
-  const VITE_ENDPOINT_CHARACTERS = import.meta.env.VITE_ENDPOINT_CHARACTERS;
-  const characters = useFetch<ApiResponse>(VITE_ENDPOINT_CHARACTERS);
+          const VITE_ENDPOINT_CHARACTERS = import.meta.env.VITE_ENDPOINT_CHARACTERS;
+          const { loading, error, data } = useFetch<ApiResponse>(VITE_ENDPOINT_CHARACTERS);
 
-  return (
-    <>
-      {characters.loading && <p>Loading...</p>}
-      {characters.error && <p>Error: {characters.error.message}</p>}
-      <br />
+          const [q, setQ] = useState<string>("");
+          const [searchParam] = useState<string[]>(["name"]);
 
-      <ul>
-        {characters.data?.results.map((character: Character) => (
-          <li key={character.id}>{character.name}</li>
-        ))}
-      </ul>
-    </>
-  );
+          function search(items: Character[]): Character[] {
+                    return items.filter((item) => {
+                              return searchParam.some((newItem) => {
+                                        return (
+                                                  item[newItem as keyof Character]
+                                                            .toString()
+                                                            .toLowerCase()
+                                                            .indexOf(q.toLowerCase()) > -1
+                                        );
+                              });
+                    });
+          }
+
+          return (
+                    <>
+                              <input
+                                        type="search"
+                                        name="search-form"
+                                        placeholder="Buscar..."
+                                        value={q}
+                                        onChange={(e) => setQ(e.target.value)}
+                              />
+                              {loading && <p>Loading...</p>}
+                              {error && <p>Error: {error.message}</p>}
+                              <br />
+                              <ul>
+                                        {data &&
+                                                  search(data.results).map((character: Character) => (
+                                                            <li key={character.id}>{character.name}</li>
+                                                  ))}
+                              </ul>
+                    </>
+          );
 };
 
 export default Characters;
